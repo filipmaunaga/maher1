3f2c3917b7e7e4e8a9dbb7392d4aebb47cbf43e9
fico <fico@maher>
2019-11-09 19:38:54 +0100

    this is the third commit

--- maher1/Program.cs
+++ maher1/Program.cs
@@ -10,18 +10,69 @@ namespace maher1
     {
         static void Main(string[] args)
         {
-            program that returns a sum of n numbers(example: n = 10):
-            int sum = 0;
-            for (int i = 1; i <= 10; i++)
+            //program that returns a sum of n numbers(example: n = 10):
+            //int sum = 0;
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    sum = sum + i;
+
+            //}
+            //Console.WriteLine("Sum is equal to: {0}", sum);
+            //Console.ReadKey();
+
+            //method that returns the larger value of two numbers
+            //Program find = new Program();
+            //int result = find.FindMax(64, 16);
+            //Console.WriteLine("The larger number is: {0}", result);
+            //Console.ReadKey();
+            //method that is a simple calculator
+            //Program calculator = new Program();
+            //Console.WriteLine("Enter the first number: ");
+            //double a = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Enter the second number: ");
+            //double b = Convert.ToDouble(Console.ReadLine());
+            //Console.WriteLine("Enter the operation: ");
+            //string o = (Console.ReadLine());
+            //double result = calculator.SimpleCalculator(a, b, o);
+            //Console.WriteLine("The result is: {0} {1} {2}={3}", a, o, b, result);
+            //Console.ReadKey();
+        }
+        public int FindMax(int a, int b)
+        {
+            int result;
+            if (a > b)
             {
-                sum = sum + i;
-
+                result = a;
             }
-            Console.WriteLine("Sum is equal to: {0}", sum);
-            Console.ReadKey();
-
-
-
+            else
+            {
+                result = b;
+            }
+            return result;
+        }
+        public double SimpleCalculator(double a, double b, string o)
+        {
+            double result;
+            switch (o)
+            {
+                case "+":
+                    result = a + b;
+                    break;
+                case "-":
+                    result = a - b;
+                    break;
+                case "*":
+                    result = a * b;
+                    break;
+                case "/":
+                    result = a / b;
+                    break;
+                 default:
+                    Console.WriteLine("Unknown operation");
+                    result = 0;
+                    break;
+            }
+            return result;
         }
     }
 }
