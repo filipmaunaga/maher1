e7433ddce2e51400d25a1f2839ce35d996aaa542
fico <fico@maher>
2019-11-09 18:44:30 +0100

    this is initial commit

---
+++ maher1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maher1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //program that returns a sum of n numbers (example: n=10):
+            //int sum = 0;
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    sum = sum+i;
+
+            //}
+            //Console.WriteLine("Sum is equal to: {0}", sum);
+            //Console.ReadKey();
+
+
+        }
+    }
+}
